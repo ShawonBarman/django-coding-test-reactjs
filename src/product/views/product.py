c2ffd0428b6d42d2471e69ec58ca457adf2c0303
ShawonBarman <shawonbarmon34@gmail.com>
2023-06-01 23:07:03 +0600

    Final commit

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,14 +1,120 @@
 from django.views import generic
-
-from product.models import Variant
+from django.core.paginator import Paginator
+from product.models import *
+from django.db.models import Q
 
 
 class CreateProductView(generic.TemplateView):
     template_name = 'products/create.html'
 
     def get_context_data(self, **kwargs):
-        context = super(CreateProductView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         variants = Variant.objects.filter(active=True).values('id', 'title')
         context['product'] = True
         context['variants'] = list(variants.all())
         return context
+
+
+class ProductListView(generic.TemplateView):
+    template_name = 'products/list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        request = self.request  # Retrieve the request object
+        # Retrieve the filter values from the request's GET parameters
+        title = request.GET.get('title')
+        variant = request.GET.get('variant')
+        price_from = request.GET.get('price_from')
+        price_to = request.GET.get('price_to')
+        date = request.GET.get('date')
+
+        # Build the filter query dynamically based on the filter values
+        filters = Q()
+        if title:
+            filters &= Q(title__icontains=title)
+        if variant:
+            filters &= Q(variants__variant_title=variant)
+        if price_from:
+            filters &= Q(variants__price__gte=float(price_from))
+        if price_to:
+            filters &= Q(variants__price__lte=float(price_to))
+        if date:
+            filters &= Q(created_at=date)
+
+        # Apply the filters to retrieve the filtered products
+        products_filter = Product.objects.filter(filters)
+        
+        products = Product.objects.all()
+        paginate_by = 10
+
+        product_data = []
+        variant_groups = {}
+        for product in products:
+            variants = ProductVariant.objects.filter(product=product)
+            variant_data = []
+            for variant in variants:
+                variant_prices = ProductVariantPrice.objects.filter(product=variant.product)
+                # print(variant_prices)
+                variant_price_data = []
+                for variant_price in variant_prices:
+                    variant_title = ""
+                    if variant_price.product_variant_one:
+                        variant_title += variant_price.product_variant_one.variant_title + " / "
+
+                    if variant_price.product_variant_two:
+                        variant_title += variant_price.product_variant_two.variant_title + " / "
+                    else:
+                        # Remove the last '/' from variant_title if it exists
+                        variant_title = variant_title.rstrip(" / ")
+                        
+                    if variant_price.product_variant_three:
+                        variant_title += variant_price.product_variant_three.variant_title
+                    else:
+                        # Remove the last '/' from variant_title if it exists
+                        variant_title = variant_title.rstrip(" / ")
+                    
+                    variant_price_data.append({
+                        'variant_title': variant_title,
+                        'price': variant_price.price,
+                        'stock': variant_price.stock
+                    })
+                    
+                    variant_group = variant.variant.title
+                    variant_options = []
+                    if variant_price.product_variant_one and variant.variant.title == 'Size':
+                        variant_options.append(variant_price.product_variant_one.variant_title)
+                    if variant_price.product_variant_two and variant.variant.title == 'Color':
+                        variant_options.append(variant_price.product_variant_two.variant_title)
+                    if variant_price.product_variant_three and variant.variant.title == 'Style':
+                        variant_options.append(variant_price.product_variant_three.variant_title)
+
+                    variant_groups.setdefault(variant_group, []).extend(variant_options)
+                    
+            for variant in variants:
+                variant_groups[variant.variant.title] = list(set(variant_groups[variant.variant.title]))
+            
+            variant_data.append({
+                'title': variant_group,
+                'options': variant_options
+            })
+            
+            product_data.append({
+                'title': product.title,
+                'created_at': product.created_at,
+                'description': product.description,
+                'variants': variant_price_data
+            })
+            
+        # Paginate the products
+        paginator = Paginator(products, paginate_by)
+        page_number = self.request.GET.get('page')
+        products_page = paginator.get_page(page_number)
+
+        context['products'] = product_data
+        context['variants'] = variant_groups
+        context['products_page'] = products_page
+        context['products_filter'] = products_filter
+            
+        return context
+    
+    
--- src/templates/assets/js/components/CreateProduct.js
+++ src/templates/assets/js/components/CreateProduct.js
@@ -1,225 +1,240 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import TagsInput from 'react-tagsinput';
 import 'react-tagsinput/react-tagsinput.css';
-import Dropzone from 'react-dropzone'
 
+// Retrieve the CSRF token from the cookies
+const csrftoken = getCookie('csrftoken');
+
+// Helper function to get the value of a cookie by name
+function getCookie(name) {
+  const cookieValue = document.cookie.match('(^|;)\\s*' + name + '=([^;]+)');
+  return cookieValue ? cookieValue.pop() : '';
+}
+
+// Add the CSRF token to Axios requests
+axios.defaults.headers.common['X-CSRFToken'] = csrftoken;
 
 const CreateProduct = (props) => {
+  const [productData, setProductData] = useState({
+    productName: '',
+    productSKU: '',
+    description: ''
+  });
 
-    const [productVariantPrices, setProductVariantPrices] = useState([])
+  const [variantData, setVariantData] = useState([]);
 
-    const [productVariants, setProductVariant] = useState([
-        {
-            option: 1,
-            tags: []
-        }
-    ])
-    console.log(typeof props.variants)
-    // handle click event of the Add button
-    const handleAddClick = () => {
-        let all_variants = JSON.parse(props.variants.replaceAll("'", '"')).map(el => el.id)
-        let selected_variants = productVariants.map(el => el.option);
-        let available_variants = all_variants.filter(entry1 => !selected_variants.some(entry2 => entry1 == entry2))
-        setProductVariant([...productVariants, {
-            option: available_variants[0],
-            tags: []
-        }])
+  // handle input change for product fields
+  const handleProductInputChange = (event) => {
+    const { name, value } = event.target;
+    setProductData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  // handle input change for variant fields
+  const handleVariantInputChange = (event, index) => {
+    const { name, value } = event.target;
+    const updatedVariants = [...variantData];
+    updatedVariants[index][name] = value;
+    setVariantData(updatedVariants);
+  };
+
+  // handle add variant
+  const handleAddVariant = () => {
+    setVariantData((prevVariants) => [
+      ...prevVariants,
+      { option: '', tags: [] }
+    ]);
+  };
+
+  // handle remove variant
+  const handleRemoveVariant = (index) => {
+    setVariantData((prevVariants) => {
+      const updatedVariants = [...prevVariants];
+      updatedVariants.splice(index, 1);
+      return updatedVariants;
+    });
+  };
+
+  // Save product and variants
+  const saveProduct = (event) => {
+    event.preventDefault();
+
+    // Prepare product data
+    const productPayload = {
+      name: productData.productName,
+      sku: productData.productSKU,
+      description: productData.description
     };
 
-    // handle input change on tag input
-    const handleInputTagOnChange = (value, index) => {
-        let product_variants = [...productVariants]
-        product_variants[index].tags = value
-        setProductVariant(product_variants)
+    // Make API call to save the product
+    axios.post('api/saveProduct', productPayload)
+      .then((response) => {
+        const productId = response.data.id;
 
-        checkVariant()
-    }
+        // Prepare variant data
+        const variantPayload = variantData.map((variant) => {
+          return {
+            option: variant.option,
+            tags: variant.tags,
+            productId: productId
+          };
+        });
 
-    // remove product variant
-    const removeProductVariant = (index) => {
-        let product_variants = [...productVariants]
-        product_variants.splice(index, 1)
-        setProductVariant(product_variants)
-    }
+        // Make API call to save variants
+        axios.post('api/saveVariant', variantPayload)
+          .then((response) => {
+            // Variant data saved successfully
+            console.log('Variants saved:', response.data);
+          })
+          .catch((error) => {
+            // Error saving variants
+            console.error('Error saving variants:', error);
+          });
+      })
+      .catch((error) => {
+        // Error saving product
+        console.error('Error saving product:', error);
+      });
 
-    // check the variant and render all the combination
-    const checkVariant = () => {
-        let tags = [];
+    // Clear the form fields
+    setProductData({
+      productName: '',
+      productSKU: '',
+      description: ''
+    });
+    setVariantData([]);
+  };
 
-        productVariants.filter((item) => {
-            tags.push(item.tags)
-        })
-
-        setProductVariantPrices([])
-
-        getCombn(tags).forEach(item => {
-            setProductVariantPrices(productVariantPrice => [...productVariantPrice, {
-                title: item,
-                price: 0,
-                stock: 0
-            }])
-        })
-
-    }
-
-    // combination algorithm
-    function getCombn(arr, pre) {
-        pre = pre || '';
-        if (!arr.length) {
-            return pre;
-        }
-        let ans = arr[0].reduce(function (ans, value) {
-            return ans.concat(getCombn(arr.slice(1), pre + value + '/'));
-        }, []);
-        return ans;
-    }
-
-    // Save product
-    let saveProduct = (event) => {
-        event.preventDefault();
-        // TODO : write your code here to save the product
-    }
-
-
-    return (
-        <div>
-            <section>
-                <div className="row">
-                    <div className="col-md-6">
-                        <div className="card shadow mb-4">
-                            <div className="card-body">
-                                <div className="form-group">
-                                    <label htmlFor="">Product Name</label>
-                                    <input type="text" placeholder="Product Name" className="form-control"/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="">Product SKU</label>
-                                    <input type="text" placeholder="Product Name" className="form-control"/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="">Description</label>
-                                    <textarea id="" cols="30" rows="4" className="form-control"></textarea>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="card shadow mb-4">
-                            <div
-                                className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                <h6 className="m-0 font-weight-bold text-primary">Media</h6>
-                            </div>
-                            <div className="card-body border">
-                                <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
-                                    {({getRootProps, getInputProps}) => (
-                                        <section>
-                                            <div {...getRootProps()}>
-                                                <input {...getInputProps()} />
-                                                <p>Drag 'n' drop some files here, or click to select files</p>
-                                            </div>
-                                        </section>
-                                    )}
-                                </Dropzone>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-md-6">
-                        <div className="card shadow mb-4">
-                            <div
-                                className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                <h6 className="m-0 font-weight-bold text-primary">Variants</h6>
-                            </div>
-                            <div className="card-body">
-
-                                {
-                                    productVariants.map((element, index) => {
-                                        return (
-                                            <div className="row" key={index}>
-                                                <div className="col-md-4">
-                                                    <div className="form-group">
-                                                        <label htmlFor="">Option</label>
-                                                        <select className="form-control" defaultValue={element.option}>
-                                                            {
-                                                                JSON.parse(props.variants.replaceAll("'", '"')).map((variant, index) => {
-                                                                    return (<option key={index}
-                                                                                    value={variant.id}>{variant.title}</option>)
-                                                                })
-                                                            }
-
-                                                        </select>
-                                                    </div>
-                                                </div>
-
-                                                <div className="col-md-8">
-                                                    <div className="form-group">
-                                                        {
-                                                            productVariants.length > 1
-                                                                ? <label htmlFor="" className="float-right text-primary"
-                                                                         style={{marginTop: "-30px"}}
-                                                                         onClick={() => removeProductVariant(index)}>remove</label>
-                                                                : ''
-                                                        }
-
-                                                        <section style={{marginTop: "30px"}}>
-                                                            <TagsInput value={element.tags}
-                                                                       style="margin-top:30px"
-                                                                       onChange={(value) => handleInputTagOnChange(value, index)}/>
-                                                        </section>
-
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        )
-                                    })
-                                }
-
-
-                            </div>
-                            <div className="card-footer">
-                                {productVariants.length !== 3
-                                    ? <button className="btn btn-primary" onClick={handleAddClick}>Add another
-                                        option</button>
-                                    : ''
-                                }
-
-                            </div>
-
-                            <div className="card-header text-uppercase">Preview</div>
-                            <div className="card-body">
-                                <div className="table-responsive">
-                                    <table className="table">
-                                        <thead>
-                                        <tr>
-                                            <td>Variant</td>
-                                            <td>Price</td>
-                                            <td>Stock</td>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        {
-                                            productVariantPrices.map((productVariantPrice, index) => {
-                                                return (
-                                                    <tr key={index}>
-                                                        <td>{productVariantPrice.title}</td>
-                                                        <td><input className="form-control" type="text"/></td>
-                                                        <td><input className="form-control" type="text"/></td>
-                                                    </tr>
-                                                )
-                                            })
-                                        }
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <button type="button" onClick={saveProduct} className="btn btn-lg btn-primary">Save</button>
-                <button type="button" className="btn btn-secondary btn-lg">Cancel</button>
-            </section>
+  return (
+    <div>
+      <form onSubmit={saveProduct}>
+        <div className="form-group">
+          <label htmlFor="productName">Product Name</label>
+          <input
+            type="text"
+            id="productName"
+            name="productName"
+            value={productData.productName}
+            onChange={handleProductInputChange}
+            className="form-control"
+            placeholder="Product Name"
+          />
         </div>
-    );
+        <div className="form-group">
+          <label htmlFor="productSKU">Product SKU</label>
+          <input
+            type="text"
+            id="productSKU"
+            name="productSKU"
+            value={productData.productSKU}
+            onChange={handleProductInputChange}
+            className="form-control"
+            placeholder="Product SKU"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            name="description"
+            value={productData.description}
+            onChange={handleProductInputChange}
+            className="form-control"
+            placeholder="Description"
+            rows="4"
+          ></textarea>
+        </div>
+
+        <div className="card shadow mb-4">
+          <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+            <h6 className="m-0 font-weight-bold text-primary">Variants</h6>
+        </div>
+        <div className="card-body">
+          {variantData.map((variant, index) => (
+            <div className="row" key={index}>
+              <div className="col-md-4">
+                <div className="form-group">
+                  <label htmlFor="">Option</label>
+                  <select
+                    className="form-control"
+                    name="option"
+                    value={variant.option}
+                    onChange={(event) => handleVariantInputChange(event, index)}
+                  >
+                    {variants.map((variantOption) => (
+                      <option key={variantOption.id} value={variantOption.id}>{variantOption.title}</option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+
+              <div className="col-md-8">
+                <div className="form-group">
+                  {variantData.length > 1 && (
+                    <label
+                      htmlFor=""
+                      className="float-right text-primary"
+                      style={{ marginTop: "-30px" }}
+                      onClick={() => handleRemoveVariant(index)}
+                    >
+                      remove
+                    </label>
+                  )}
+
+                  <section style={{ marginTop: "30px" }}>
+                    <TagsInput
+                      value={variant.tags}
+                      onChange={(value) =>
+                        handleVariantInputChange(
+                          { target: { name: "tags", value } },
+                          index
+                        )
+                      }
+                    />
+                  </section>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="card-footer">
+          {variantData.length !== 3 && (
+            <button
+              className="btn btn-primary"
+              onClick={handleAddVariant}
+            >
+              Add another option
+            </button>
+          )}
+        </div>
+      </div>
+
+      <div className="card-header text-uppercase">Preview</div>
+      <div className="card-body">
+        <div className="table-responsive">
+          <table className="table">
+            <thead>
+              <tr>
+                <td>Variant</td>
+                <td>Price</td>
+                <td>Stock</td>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Render your variant prices here */}
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <button type="submit" className="btn btn-lg btn-primary">Save</button>
+      <button type="button" className="btn btn-secondary btn-lg">Cancel</button>
+    </form>
+  </div>
+  );
 };
 
 export default CreateProduct;
